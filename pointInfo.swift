36dc1de2d2141a48cf77cd916e37c53183a3e218
xujk <kevin16021777@gmail.com>
2024-01-30 22:32:34 +0800

    feat:point and symptom infomation, symptom image

--- pointInfo.swift
+++ pointInfo.swift
@@ -10,36 +10,73 @@ import Foundation
 struct Point: Equatable, Identifiable {
     let id = UUID()
     var name: String
-    var healPart: String
+    var code: Double
     var symptom: String
     var lacation: String
     var detail: String
-    var image: String
-    var messageTime: Double
+    var messageTime: String
     
     static let hexamples = [
-        Point(name: "Yongquan acupoint (湧泉穴)", healPart: "head", symptom: "Insomnia", lacation: "1502", detail: "wwwwwww", image: "1030", messageTime: 10),
-        Point(name: "湧泉", healPart: "head", symptom: "Ins", lacation: "feet", detail: "wwwwwww", image: "194", messageTime: 10),
-        Point(name: "湧穴", healPart: "head", symptom: "nia", lacation: "feet", detail: "wwwwwww", image: "194", messageTime: 10)
+        Point(name: "Yongquan acupoint (印堂穴)", code: 1, symptom: "Insomnia", lacation: "1502", detail: "wwwwwww", messageTime: "10"),
+        Point(name: "Yongquan acupoint (太陽穴)", code: 2, symptom: "Insomnia", lacation: "1502", detail: "wwwwwww", messageTime: "10"),
+        Point(name: "Yongquan acupoint (風池穴)", code: 3, symptom: "Insomnia", lacation: "1502", detail: "wwwwwww", messageTime: "10"),
+        Point(name: "Yongquan acupoint (百會穴)", code: 13, symptom: "Insomnia", lacation: "1502", detail: "wwwwwww", messageTime: "10"),
     ]
     static let bexamples = [
-        Point(name: "湧泉穴", healPart: "body", symptom: "Insomnia", lacation: "feet", detail: "wwwwwww", image: "119", messageTime: 10)
+        Point(name: "Yongquan acupoint (天樞穴)", code: 4, symptom: "Insomnia", lacation: "1502", detail: "wwwwwww", messageTime: "10"),
+        Point(name: "Yongquan acupoint (中脘穴)", code: 5, symptom: "Insomnia", lacation: "1502", detail: "wwwwwww", messageTime: "10"),
+        Point(name: "Yongquan acupoint (腰眼穴)", code: 12, symptom: "Insomnia", lacation: "1502", detail: "wwwwwww", messageTime: "10"),
     ]
-    static let oexamples = [
-        Point(name: "湧泉穴", healPart: "other", symptom: "Insomnia", lacation: "feet", detail: "wwwwwww", image: "120", messageTime: 10)
+    static let haexamples = [
+        Point(name: "Yongquan acupoint (合谷穴)", code: 6, symptom: "Insomnia", lacation: "1502", detail: "wwwwwww", messageTime: "10"),
+        Point(name: "Yongquan acupoint (落枕穴)", code: 7, symptom: "Insomnia", lacation: "1502", detail: "wwwwwww", messageTime: "10"),
+        Point(name: "Yongquan acupoint (腰腿點穴)", code: 8, symptom: "Insomnia", lacation: "1502", detail: "wwwwwww", messageTime: "10"),
+        Point(name: "Yongquan acupoint (內關穴)", code: 9, symptom: "Insomnia", lacation: "1502", detail: "wwwwwww", messageTime: "10"),
+    ]
+    static let fexamples = [
+        Point(name: "Yongquan acupoint (湧泉穴)", code: 10, symptom: "Insomnia", lacation: "1502", detail: "wwwwwww", messageTime: "10"),
+        Point(name: "Yongquan acupoint (足三里穴)", code: 11, symptom: "Insomnia", lacation: "1502", detail: "wwwwwww", messageTime: "10"),
     ]
     
+}
+
+struct Symptom: Equatable, Identifiable {
+    let id = UUID()
+    var symptom: String
+    var pointCode: String
+    var image: String
+    var exDetail: String
     
-    static let headPoint = [
-        Point(name: "湧泉穴", healPart: "other", symptom: "Insomnia", lacation: "feet", detail: "wwwwwww", image: "120", messageTime: 10)
+    static let hSymExamples = [
+        Symptom(symptom: "headache", pointCode: "A", image: "29", exDetail: ""),
+        Symptom(symptom: "stiff neck", pointCode: "B", image: "399", exDetail: ""),
     ]
-    static let bodyPoint = [
-        Point(name: "湧泉穴", healPart: "other", symptom: "Insomnia", lacation: "feet", detail: "wwwwwww", image: "120", messageTime: 10)
+    static let bSymExamples = [
+        Symptom(symptom: "lower back pain/sore", pointCode: "C", image: "1023", exDetail: ""),
+        Symptom(symptom: "stomach bloating and pain", pointCode: "D", image: "193", exDetail: ""),
+        Symptom(symptom: "palpitations", pointCode: "E", image: "191", exDetail: ""),
+        Symptom(symptom: "strained lower back", pointCode: "K", image: "1022", exDetail: ""),
     ]
-    static let handPoint = [
-        Point(name: "湧泉穴", healPart: "other", symptom: "Insomnia", lacation: "feet", detail: "wwwwwww", image: "120", messageTime: 10)
-    ]
-    static let feetPoint = [
-        Point(name: "湧泉穴", healPart: "head", symptom: "Insomnia", lacation: "feet", detail: "wwwwwww", image: "1030", messageTime: 10),
+    static let oSymExamples = [
+        Symptom(symptom: "Insomnia", pointCode: "F", image: "1030", exDetail: ""),
+        Symptom(symptom: "anxiety", pointCode: "G", image: "1447", exDetail: ""),
+        Symptom(symptom: "constipation", pointCode: "H", image: "192", exDetail: ""),
+        Symptom(symptom: "nausea", pointCode: "I", image: "34", exDetail: ""),
+        Symptom(symptom: "muscle soreness", pointCode: "J", image: "1104", exDetail: ""),
     ]
 }
+
+// code table:
+ /*
+  A: 1, 2, 3
+  B: 7
+  C: 8, 12
+  D: 5, 9, 11
+  E: 9
+  F: 6, 9, 10
+  G: 9, 10. 13, 1
+  H: 4
+  I: 5, 9
+  J: 6, 9, 11
+  K: 8
+ */
